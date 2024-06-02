c4c60c731709c33a7d80b0273d4be2f86e845a81
JackTylerIsReallyCool <JackTyler168@gmail.com>
2024-06-02 14:26:16 +0100

    Began move validation

--- chess.py
+++ chess.py
@@ -12,6 +12,9 @@ WHITE_KING = "♔"
 WHITE_PAWN = "♙"
 EMPTY = " "
 
+WHITE_PIECES = {WHITE_PAWN, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK, WHITE_QUEEN, WHITE_KING}
+BLACK_PIECES = {BLACK_PAWN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, BLACK_QUEEN, BLACK_KING}
+
 PIECE_SYMBOLS = {
     'r': BLACK_ROOK,
     'n': BLACK_KNIGHT,
@@ -61,3 +64,82 @@ def format_board(board):
     formatted_board.append(column_labels)
     
     return "\n".join(formatted_board)
+
+
+def square_to_indices(square):
+    column, row = square
+    row_index = 8 - int(row)
+    col_index = ord(column) - ord('a')
+    return row_index, col_index
+
+def validate_move(current_board_fen, starting_square, target_square, active_colour):
+    
+    #Validate that the move is well-formed
+    if starting_square == target_square:
+        return False
+    
+    if len(starting_square) != 2 or len(target_square) != 2:
+        return False
+    
+    starting_square_indices = square_to_indices(starting_square)
+    target_square_indices = square_to_indices(target_square)
+    
+    #Rewrite this using range
+    if int(starting_square_indices[0]) > 7 or int(starting_square_indices[1] > 7):
+        return False
+    
+    if int(starting_square_indices[0]) < 0 or int(starting_square_indices[1] < 0):
+        return False
+    
+    if int(target_square_indices[0]) > 7 or int(target_square_indices[1] > 7):
+        return False
+    
+    if int(target_square_indices[0]) < 0 or int(target_square_indices[1] < 0):
+        return False
+    
+    current_board = board_from_fen(current_board_fen)
+    
+    starting_square_piece = current_board[starting_square_indices[0]][starting_square_indices[1]]
+    target_square_piece = current_board[target_square_indices[0]][target_square_indices[1]]
+    
+    if starting_square_piece == EMPTY:
+        return False
+    
+    if active_colour == 'w':
+        if starting_square_piece in BLACK_PIECES:
+            return False
+        
+        if target_square_piece in WHITE_PIECES:
+            return False
+    
+    if active_colour == 'b':
+        if starting_square_piece in WHITE_PIECES:
+            return False
+        
+        if target_square_piece in BLACK_PIECES:
+            return False
+        
+    if starting_square_piece == WHITE_PAWN:
+        #Cannot change file unless capturing
+        if target_square_piece == EMPTY and starting_square_indices[1] != target_square_indices[1]:
+            return False
+        #Cannot capture on 2 forward move
+        if target_square_piece != EMPTY and starting_square_indices[1] != target_square_indices[1] and starting_square_indices[0] - target_square_indices[0] != 1:
+            return False
+        #Cannot move 2 forward unless on starting rank
+        if starting_square_indices[0] - target_square_indices[0] > 2:
+            return False
+        #Cannot move more than 1 forward otherwise
+        if starting_square_indices[0] - target_square_indices[0] > 1 and starting_square_indices[0] != 6:
+            return False
+        #Cannot move backwards
+        if starting_square_indices[0] < target_square_indices[0]:
+            return False
+        
+        
+    
+    print(current_board[starting_square_indices[0]][starting_square_indices[1]], current_board[target_square_indices[0]][target_square_indices[1]])
+    
+    print(starting_square, target_square, starting_square_indices, target_square_indices)
+    
+    return True
--- test_chess.py
+++ test_chess.py
@@ -39,3 +39,42 @@ def test_format_board():
     )
     
     assert formatted_board == expected_output
+
+def test_square_to_indices():
+    all_squares = [f"{col}{row}" for col in "abcdefgh" for row in "12345678"]
+    
+    for square in all_squares:
+        row_index, col_index = square_to_indices(square)
+        assert row_index < 8 and row_index >= 0
+        assert col_index < 8 and col_index >= 0
+
+def test_move_validtion():
+    ranks = ['a','b','c','d','e','f','g','h','i']
+    files = ['1','2','3','4','5','6','7','8','9']
+    
+    test_cases = [
+        ["8/8/8/8/8/8/P7/8",2],
+        ["8/8/8/8/8/P7/8/8",1],
+        ["8/8/8/8/1p6/P7/8/8",2],
+        ["8/8/8/8/1p6/8/P7/8",2],
+        ["8/8/8/8/8/P7/P7/8",1],
+        [STARTING_FEN, 20]
+        ]
+    
+    current_fen = STARTING_FEN
+    
+    for test_case in test_cases:
+        print("Testing", test_case[0])
+        count_valid_moves = 0
+        for start_rank in ranks:
+            for start_file in files:
+                for target_rank in ranks:
+                    for target_file in files:
+                        starting_square = start_rank + start_file
+                        target_square = target_rank + target_file
+                                            
+                        if validate_move(test_case[0], starting_square, target_square, 'w'):
+                            count_valid_moves += 1
+        
+        assert count_valid_moves == test_case[1]
+        print("Passed")
